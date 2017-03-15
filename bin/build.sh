25cb95ce32849a5604a5de70b370311e145c8ce2
Barry Hoggard <barry@tristanmedia.com>
2017-03-15 15:53:35 -0400

    add jq to base image

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -e
+
 DIR=`pwd`
 
-for IMAGE in alpine-glibc alpline-oraclejdk8 alpine-scala
+for IMAGE in alpine-glibc alpine-oraclejdk8 alpine-scala
 do
   cd $DIR/$IMAGE
   docker build -t artsy/$IMAGE .
