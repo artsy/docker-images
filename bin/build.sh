d514bbd0c1bf81e62d92aefbbb1ef0125acd11cb
ashkan18 <ashkan18@gmail.com>
2017-07-13 09:02:19 -0400

    Add ruby-2-4-1-qt5 image and update readme

--- bin/build.sh
+++ bin/build.sh
@@ -4,7 +4,7 @@ set -e
 
 DIR=`pwd`
 
-for IMAGE in alpine-glibc alpine-oraclejdk8 alpine-scala
+for IMAGE in alpine-glibc alpine-oraclejdk8 alpine-scala ruby-2-3-3-with-qt5 ruby-2-4-1-with-qt5
 do
   cd $DIR/$IMAGE
   docker build -t artsy/$IMAGE .
